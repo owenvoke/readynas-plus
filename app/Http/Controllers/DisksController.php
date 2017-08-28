a787d1c670709df6567426f6faae53816dded882
PXgamer <owzie123@gmail.com>
2017-08-28 14:57:07 +0100

    Added auth middleware for DisksController

--- app/Http/Controllers/DisksController.php
+++ app/Http/Controllers/DisksController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class DisksController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $sys = new System();
